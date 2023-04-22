ecbf8ad134d473d9fc1edd438a225e99d52f8fdd
Pavel Sušický <me@frontendista.cz>
2023-04-22 15:10:37 +0200

    chore: add nodemon

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,37 @@
-import { Client, GatewayIntentBits } from "discord.js"
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js"
 import { config } from "dotenv"
 
 config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 
-client.on('ready', () => {
+client.commands = new Collection();
+
+client.on(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) {
+        return;
+    }
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
     }
 });
 
