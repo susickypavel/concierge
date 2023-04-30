39197d04031aa4949ac689d239841916e57ce688
Pavel Sušický <susicky.pavel@protonmail.com>
2023-04-30 10:45:23 +0200

    test(unit): add TrackQueue tests

---
+++ BotTests/TrackQueueTest.cs
@@ -0,0 +1,80 @@
+using Bot.Entities;
+using Discord;
+using Moq;
+using Victoria.Player;
+
+namespace BotTests;
+
+[TestClass]
+public class TrackQueueTest
+{
+    private ExtendedLavaTrack GetTrack()
+    {
+        var user = new Mock<IUser>();
+        var track = new Mock<LavaTrack>();
+
+        return new ExtendedLavaTrack(track.Object, user.Object);
+    }
+    
+    [TestMethod]
+    public void Should_Dequeue_On_Empty_Queue()
+    {
+        var queue = new TrackQueue();
+        
+        var isSuccessful = queue.TryDequeue(out var track);
+        
+        Assert.IsFalse(isSuccessful);
+        Assert.IsNull(track);
+    }
+
+    [TestMethod]
+    public void Should_Enqueue_New_Track()
+    {
+        var queue = new TrackQueue();
+        
+        queue.Enqueue(GetTrack());
+        
+        Assert.IsFalse(queue.IsEmpty());
+    }
+    
+    [TestMethod]
+    public void Should_Successfully_Dequeue()
+    {
+        var queue = new TrackQueue();
+        var expectedTrack = GetTrack();
+        
+        queue.Enqueue(expectedTrack);
+        
+        var isSuccessful = queue.TryDequeue(out var nextTrack);
+        
+        Assert.IsTrue(isSuccessful);
+        Assert.AreEqual(expectedTrack, nextTrack);
+    }
+
+    [TestMethod]
+    public void Should_Throw_On_Null_Track_Enqueue()
+    {
+        var queue = new TrackQueue();
+
+        Assert.ThrowsException<ArgumentNullException>(() =>  queue.Enqueue(null));
+    }
+
+    [TestMethod]
+    public void Should_Enqueue_On_Top()
+    {
+        var queue = new TrackQueue();
+        
+        queue.Enqueue(GetTrack());
+        queue.Enqueue(GetTrack());
+        queue.Enqueue(GetTrack());
+
+        var expectedTrack = GetTrack();
+        
+        queue.Enqueue(expectedTrack, true);
+
+        var isSuccessful = queue.TryDequeue(out var nextTrack);
+        
+        Assert.IsTrue(isSuccessful);
+        Assert.AreEqual(expectedTrack, nextTrack);
+    }
+}
