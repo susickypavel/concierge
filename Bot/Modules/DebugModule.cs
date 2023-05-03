2dc04a7cb646e2fdadd1fd891e0add4eff807909
Pavel Sušický <me@frontendista.cz>
2023-05-03 08:31:27 +0200

    feat(debug): unregister DebugModule in production build
    Closes #17

---
+++ Bot/Modules/DebugModule.cs
@@ -0,0 +1,33 @@
+﻿#if DEBUG
+using System.Text;
+using Discord.Interactions;
+
+namespace Bot.Modules;
+
+public class DebugModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly InteractionService _interaction;
+
+    public DebugModule(InteractionService interaction)
+    {
+        _interaction = interaction;
+    }
+
+    [SlashCommand("commands-register", "Register commands through Discord's API")]
+    public async Task RegisterCommands()
+    {
+        await _interaction.AddCommandsToGuildAsync(Context.Guild.Id, true);
+        var commands = await _interaction.RegisterCommandsToGuildAsync(Context.Guild.Id);
+
+        var builder = new StringBuilder($@"Registered {commands.Count} commands.
+");
+        
+        foreach (var command in commands)
+        {
+            builder.AppendLine($"- {command.Name} ({command.IsEnabledInDm})");
+        }
+        
+        await RespondAsync(builder.ToString(), ephemeral: true);
+    }
+}
+#endif
