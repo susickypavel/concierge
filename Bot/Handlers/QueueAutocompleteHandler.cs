825fe68e5095fe5a69c4850d143d3ba96ce5d13a
Pavel Sušický <me@frontendista.cz>
2023-05-13 15:50:46 +0200

    feat(autocomplete): filter queue results using current parameter value

--- Bot/Handlers/QueueAutocompleteHandler.cs
+++ Bot/Handlers/QueueAutocompleteHandler.cs
@@ -6,23 +6,24 @@ using Victoria.Node;
 
 namespace Bot.Handlers;
 
-// TODO: Filter based on input
-
 public class QueueAutocompleteHandler : AutocompleteHandler
 {
-    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context,
+    public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context,
         IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
     {
         var lavaNode = services.GetRequiredService<LavaNode<ExtendedLavaPlayer, ExtendedLavaTrack>>();
 
-        if (lavaNode.TryGetPlayer(context.Guild, out var player))
+        if (!lavaNode.TryGetPlayer(context.Guild, out var player))
         {
-            var autocompletion = player.TrackQueue
-                .Select((track, i) => new AutocompleteResult($"{i + 1}. {track.Title}", i));
-            
-            return AutocompletionResult.FromSuccess(autocompletion.Take(25));
+            return Task.FromResult(AutocompletionResult.FromError(InteractionCommandError.Exception, "Oops."));
         }
 
-        return AutocompletionResult.FromError(InteractionCommandError.Exception, "Oops.");
+        var value = autocompleteInteraction.Data.Current.Value.ToString();
+
+        var autocompletion = player.TrackQueue
+            .Select((track, i) => new AutocompleteResult($"{i + 1}. {track.Title}", i))
+            .Where(autocomplete => value == null || autocomplete.Name.Contains(value));
+
+        return Task.FromResult(AutocompletionResult.FromSuccess(autocompletion.Take(25)));
     }
 }
